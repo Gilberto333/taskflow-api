5ac4e720f07393c81625355264a9f77d2e5413c9
Gilberto <gilberto.d.fernandes6@aluno.senai.br>
2026-09-21 09:24:50 -0300

    vai agora

--- src/routes/tarefas.Routes.js
+++ src/routes/tarefas.Routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const tarefasController = require("../controllers/tarefasController");
+const tarefasController = require("../controllers/tarefas.Controller");
 const validar = require("../middlewares/validar");
-const schemas = require("../middlewares/schemas"); // Corrigido 'schemar' para 'schemas'
+const schemas = require("../middlewares/schemar"); // Corrigido 'schemar' para 'schemas'
 
 router.get("/estatisticas", tarefasController.estatisticas);
 router.get("/", tarefasController.listar);
